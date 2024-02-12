fd5b36160e9fc4a1045cd6a9e73a668c3bbd8a30
gusche85 <gishjode@gmail.com>
2024-02-13 01:09:45 +0800

    Category text-center

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function NutritionalInfo({nutritionInfo}) {
   return (
     <div>
-      <p>Calories: {nutritionInfo.calories} kcal</p>
-      <p>Protein: {nutritionInfo.protein} g</p>
-      <p>Carbs: {nutritionInfo.carbs} g</p>
-      <p>Fat: {nutritionInfo.fat} g</p>
+      <p>Calories: {nutritionInfo.calories} kcal  Protein: {nutritionInfo.protein} g  Carbs: {nutritionInfo.carbs} g  Fat: {nutritionInfo.fat} g</p>
     </div>
   );
 }
 
-function MenuItem({name, price, nutritionInfo}) {
+function Ingredients({ingredients}){
+  return (
+    <>
+      <p className='text-decoration-underline'>Ingredients:</p>
+      <p>{ingredients}</p>
+    </>
+  )
+}
+
+function MenuItem({name, price, ingredients, nutritionInfo}) {
   return (
     <>
      <p>
@@ -28,7 +34,7 @@ function MenuItem({name, price, nutritionInfo}) {
 function Category({ title, foods}) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2 className='text-center'>{title}</h2>
       {foods}
     </div>
   );
@@ -68,7 +74,8 @@ function App() {
     <>
     <div className="m-5 justify-content-center align-items-center">
 <main className='border border-black rounded'>
-    <h1 className='text-center p-3'> Aunty Fried Chicken</h1>
+    <h1 className='text-center pt-3 pb-0'>Welcome to Texan Hut
+    <img src='/line.png'></img></h1>
     <Category title={"Fried Chicken"} foods={friedChicken} />
     <Category title={"Desserts"} foods={desserts} />
     </main>
