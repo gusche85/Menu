33a0cf9f41a0ca80a490a010efc511f8314b8d3c
gusche85 <gishjode@gmail.com>
2024-02-13 01:50:13 +0800

    Add Ingredients component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function NutritionalInfo({nutritionInfo}) {
   return (
-    <div>
+    <div className='pt-2'>
       <p>Calories: {nutritionInfo.calories} kcal  Protein: {nutritionInfo.protein} g  Carbs: {nutritionInfo.carbs} g  Fat: {nutritionInfo.fat} g</p>
     </div>
   );
@@ -13,8 +13,8 @@ function NutritionalInfo({nutritionInfo}) {
 function Ingredients({ingredients}){
   return (
     <>
-      <p className='text-decoration-underline'>Ingredients:</p>
-      <p>{ingredients}</p>
+      <div className='text-decoration-underline'>Ingredients:</div>
+      <div>{ingredients}</div>
     </>
   )
 }
@@ -23,8 +23,8 @@ function MenuItem({name, price, ingredients, nutritionInfo}) {
   return (
     <>
      <p>
-      <strong>{name}</strong> | {price}
-      
+      <h5><strong>{name}</strong> | {price}</h5>
+      <Ingredients ingredients={ingredients} />
       <NutritionalInfo nutritionInfo={nutritionInfo} />
      </p>
     </>
@@ -45,11 +45,13 @@ const friedChicken = (
     <MenuItem 
     name={"2pc Fried Chicken Set"} 
     price={"RM 10.00"} 
+    ingredients={"Chicken, salt, pepper, chilli powder"}
     nutritionInfo={{calories: 540, protein: 31, carbs: 35, fat: 29}}/>
 
     <MenuItem 
     name={"6 piece nugget"} 
     price={"RM 6.00"} 
+    ingredients={"Chicken nuggets, salt, pepper, mayonnaise"}
     nutritionInfo={{calories: 270, protein: 13, carbs: 16, fat: 16}}/>
   </>
 );
@@ -59,11 +61,13 @@ const desserts = (
     <MenuItem 
     name={"Ice Cream"} 
     price={"RM 5.00"} 
+    ingredients={"Chocolate ice cream, crushed nuts"}
     nutritionInfo={{calories: 207, protein: 2, carbs: 33, fat: 6}}/>
 
     <MenuItem 
     name={"Apple Pie"} 
     price={"RM 2.00"} 
+    ingredients={"Apples, cinnamon powder, sugar, flour, salt"}
     nutritionInfo={{calories: 270, protein: 1, carbs: 43, fat: 10}}/>
   </>
 );
