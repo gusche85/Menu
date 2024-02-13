e61cda9771e912429017c1727a058209f7b4c7d0
gusche85 <gishjode@gmail.com>
2024-02-13 23:06:30 +0800

    carousel fade

--- src/App.jsx
+++ src/App.jsx
@@ -79,26 +79,23 @@ function CategorySlide({ title, foods, active }) {
 
 function CategoryCarousel({ categories }) {
   return (
-    <div id='carouselExampleIndicators' className='carousel slide'>
-    <div className='carousel-indicators'>
-      {categories.map((_, index) => (
-        <button key={index} type="button" data-bs-slide-to={index} data-bs-target="#carouselExampleIndicators" className={index === 0 ? 'active' : ''}></button>
-      ))}
-    </div>
+    <div className="container d-flex justify-content-center align-items-center">
+    <div id='carouselExampleFade' className='carousel slide carousel-fade'>
+    
     <div className='carousel-inner'>
       {categories.map((category, index) => (
         <CategorySlide key={index} title={category.title} foods={category.foods} active={index === 0} />
       ))}
     </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Previous</span>
     </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
       <span className="carousel-control-next-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Next</span>
     </button>
-  </div>
+  </div></div>
   );
 }
 
