7a4ef5182ab1ba2baf5567b02d7df6ac2e6511f2
gusche85 <gishjode@gmail.com>
2024-02-13 18:39:13 +0800

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css'
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 
 function NutritionalInfo({nutritionInfo}) {
