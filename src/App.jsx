55a73bbad786ffae5055d973ba4279eb167f918c
gusche85 <gishjode@gmail.com>
2024-02-15 01:26:19 +0800

    touchup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './index.css'
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
